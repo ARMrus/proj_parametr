4795de76cd1e5c8e9be0ec294cb26bd767c78707
Azarenko Alex <azarenko.al@gmail.com>
2020-07-06 09:37:11 +0300

    Upload New File

---
+++ test.geojson.js
@@ -0,0 +1,10 @@
+var check_points = [
+    {
+      "type": "Point",
+      "coordinates": [30.1, 59.8338730825]
+    },
+    {
+      "type": "Point",
+      "coordinates": [30.29, 59.83387308251]
+    }];
+  
